d4db070c047f5c6353627052901bbbf19c09246c
Gopika1 <74651932+Gopika1@users.noreply.github.com>
2021-05-26 10:18:17 +0530

    Add files via upload

--- StudentMarks.java
+++ StudentMarks.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
-public class Stud_Marks
+
+class StudentMarks
 {
-    public static void main(String[] args) 
-    {
-        int n, total = 0, p;
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of subject:");
-        n = s.nextInt();
-        int marks[] = new int[n];
-        System.out.println("Enter marks out of 100:");
-        for(int i = 0; i < n; i++)
-        {
-            marks[i] = s.nextInt();
-            total = total + marks[i];
-        }
-        p = total / n;
-        System.out.println("Sum:"+total);
-        System.out.println("Percentage:"+p);
-    }
+public static void main(String args[])
+{
+     float sub1,sub2,sub3,sub4,sub5; 
+     double total, percentage;
+    Scanner op=new Scanner(System.in);
+    
+    System.out.println("Enter marks of five subjects");
+    System.out.print("Enter marks of subject1:");
+    sub1=op.nextFloat();
+    System.out.print("Enter marks of subject2:");
+    sub2=op.nextFloat();
+    System.out.print("Enter marks of subject3:");
+    sub3=op.nextFloat();
+    System.out.print("Enter marks of subject4:");
+    sub4=op.nextFloat();
+    System.out.print("Enter marks of subject5:");
+    sub5=op.nextFloat();
+
+   
+    total = sub1+sub2+sub3+sub4+sub5;
+    percentage = (total / 500) * 100;
+
+  
+    System.out.println("Total marks ="+total);
+    System.out.println("Percentage = "+percentage);
+
+   }
 }
