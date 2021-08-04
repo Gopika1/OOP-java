f526d7f1ac2f11d2b6cfd6db39d7a6d19341cb1a
Gopika1 <74651932+Gopika1@users.noreply.github.com>
2021-08-04 14:14:00 +0530

    Update StringManipulation.java

--- CYCLE-2/StringManipulation.java
+++ CYCLE-2/StringManipulation.java
@@ -1,46 +1,96 @@
-public class StringManipulation
-{  
-public static void main(String args[])
-{  
-String s1="Orchid";
-char ch[]={'j','a','s','m','i','n','e'};  
+import java.util.*;
 
-String s2=new String(ch);
-String s3=new String("rose");
-System.out.println("string s1 : " +s1);  
-System.out.println("string s2 : " +s2);  
-System.out.println("string s3 : " +s3);  
+class StringManipulation
+{
+public static void main(String args[]) 
+{
+String str,str1,str2,str3,r,t;
+char c;
+int p,q,ch,n;
+Scanner s = new Scanner(System.in);
+System.out.println("***STRING MANIPULATION***");
+System.out.println("\n1.create new string\n2.getting a string length\n3.string concatenation\n4.character extraction\n5.string comparision\n6.searching substrings\n7.modifying a string\n8.data conversion using valueOf()");
 
-System.out.println("//  METHOD -  length()");
-int length =s2.length();
-System.out.println("The length of the String \""+s2+"\" is: " +length);
+do
+{
+System.out.println("enter your choice :");
+ch=s.nextInt();
+switch(ch)
+{
+case 1:
+System.out.println("enter any string : ");
+str=s.next();
+String s1 = new String(str);
+System.out.println("new string : "+s1);
+break;
+	
+case 2:
+System.out.println("enter any string : ");
+str=s.next();
+System.out.println("length of string : " + str.length());
+break;
+	
+case 3:
+System.out.println("enter 1st string : ");
+str1=s.next();
+System.out.println("enter 2nd string : ");
+str2=s.next();
+str3=str1.concat(str2);
+System.out.println("concatenation using concat() : "+str3);
+break;
+	
+case 4:
+System.out.println("enter any string : ");
+str=s.next();
+System.out.println("enter the position :");
+p=s.nextInt();
+c=str.charAt(p);
+System.out.println("character extraction : "+c);
+break;
+	
+case 5:
+System.out.println("enter 1st string : ");
+str1=s.next();
+System.out.println("enter 2nd string : ");
+str2=s.next();
+System.out.println("string comparsion : "+str1.compareTo(str2));
+break;
+	
+case 6:
+System.out.println("enter any string : ");
+str=s.next();
+System.out.println("enter the position of substring :");
+q=s.nextInt();
+System.out.println("searching substrings : "+str.substring(q));
+break;
+	
+case 7:
+System.out.println("enter any string : ");
+str1=s.next();
+System.out.println("enter the character to be replaced :");
+r=s.next();
+System.out.println("enter the character to be replaced with :");
+t=s.next();
+str2=str1.replace(r,t);
+System.out.println("replacing strings : "+str2);
+break;
 
-System.out.println("//  METHOD -  concat()");
-s2=s2.concat(" is a white flower. ");  
-System.out.println(s2);
+case 8:
+System.out.println("enter any number : ");
+n=s.nextInt();
+str=String.valueOf(n);
+System.out.println("data conversion using valueOf() : "+str);
+break;
 
-System.out.println("//  METHOD - charAt()");
-char s=s1.charAt(2);
-System.out.println("Character at position 2 of the string \""+s1+"\" is :  " + s);
+case 9:
+System.out.println("exiting!!");
+break;
 
-System.out.println("//  METHOD - compareTo()");
-System.out.println(s1.compareTo(s2));
-
-System.out.println("//  METHOD - indexOf() , lastIndexOf()");
-System.out.println("The string is : "+s2);
-int firstIndex = s2.indexOf('a');
-System.out.println("First occurrence of char 'a'  is found at : " + firstIndex);
-
-int lastIndex = s2.lastIndexOf('a');
-System.out.println("Last occurrence of char 'a' is found at : " + lastIndex);
-
-System.out.println("//  METHOD - replace()");
-System.out.println("Initial string is : "+s3);
-String replaceString=s3.replace('r','n'); 
-System.out.println("String after replace : "+replaceString); 
-
-System.out.println("//  METHOD - valueOf()");
-String  s4 = String.valueOf(ch, 0, 3);
-System.out.println(s4);
+default:
+System.out.println("invalid choice !!!");
+break;
+}
+}
+while(ch!=9);
 }
 }
